0e00736743802f6d3849c99ed5fef08a6c087e08
msardary <sardary.mostafa@gmail.com>
2025-05-07 18:03:57 +0300

    change on db connection

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"context"
 	"user-management/internal/config"
 	"user-management/internal/db"
-	"user-management/internal/server"
 	dbg "user-management/internal/db/generated"
+	"user-management/internal/server"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -17,7 +18,13 @@ func main() {
 		log.Fatal("Failed to connect to database: ", err)
 	}
 
-	queries := dbg.New(dbConn)
+	conn, err := dbConn.Acquire(context.Background())
+	if err != nil {
+		log.Fatal("Failed to acquire connection: ", err)
+	}
+	defer conn.Release()
+	
+	queries := dbg.New(conn)
 
 	if err := server.Start(queries); err != nil {
 		log.Fatal("Failed to start server: ", err)	
